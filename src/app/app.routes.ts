e4631c5758107819ed7439c63a667a85e9af3b17
GonzaQuedena <zquedena2003@gmail.com>
2024-06-05 14:21:26 -0500

    feat: adding auth and task routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  
+  { 
+    path:'auth',
+    loadChildren: () => 
+      import('@modules/auth/auth.module').then((m) => m.AuthModule)
+  },
+
+  {
+    path: 'task',
+    loadChildren: () => 
+      import('@modules/task/task.module').then((m) => m.TaskModule)
+  }
+
+
+];
